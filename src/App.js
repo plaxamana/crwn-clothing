8047bf5503c8c7c0416207fc3988b164ae4286a6
Philip Laxamana <philip.laxamana@achievers.com>
2022-11-17 08:45:49 -0500

    added navigation component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
-function App() {
+export default function App() {
+  const Shop = () => <h1>Shop</h1>;
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
-
-export default App;
